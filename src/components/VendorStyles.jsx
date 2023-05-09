1b1ddd99d066d4b617dac6e8a6faa36e033a47db
Denis-Hish <denis.hish@gmail.com>
2023-05-09 18:21:12 +0200

    Moved vendor style imports to a single file

---
+++ src/components/VendorStyles.jsx
@@ -0,0 +1,16 @@
+// Vendor CSS Files
+import './../vendor/fontawesome-free/css/all.min.css';
+import './../vendor/animate.css/animate.min.css';
+import './../vendor/bootstrap/css/bootstrap.min.css';
+import './../vendor/bootstrap-icons/bootstrap-icons.css';
+import './../vendor/boxicons/css/boxicons.min.css';
+import './../vendor/glightbox/css/glightbox.min.css';
+import './../vendor/remixicon/remixicon.css';
+import './../vendor/swiper/swiper-bundle.min.css';
+
+// Vendor JS Files
+import '../vendor/purecounter/purecounter_vanilla.js';
+import '../vendor/bootstrap/js/bootstrap.bundle.min.js';
+import '../vendor/glightbox/js/glightbox.min.js';
+import '../vendor/swiper/swiper-bundle.min.js';
+import '../vendor/php-email-form/validate.js';
