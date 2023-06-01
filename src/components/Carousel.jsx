07e9874eb65d61ec5bdb2ff3aa39d9103c640676
Denis-Hish <denis.hish@gmail.com>
2023-06-01 17:02:50 +0200

    Carousel component fix

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-import Card from 'react-bootstrap/Card';
 
 import CarouselImg1 from './../img/carousel/carousel-1.jpg';
 import CarouselImg2 from './../img/carousel/carousel-2.jpg';
@@ -10,7 +9,7 @@ import CarouselImg4 from './../img/carousel/carousel-4.jpg';
 function CarouselComponent() {
    const [index, setIndex] = useState(0);
 
-   const handleSelect = (selectedIndex) => {
+   const handleSelect = selectedIndex => {
       setIndex(selectedIndex);
    };
 
@@ -27,34 +26,55 @@ function CarouselComponent() {
          interval="10000" // интервал смены слайдов в мс
       >
          <Carousel.Item>
-            <img className="d-block w-100" src={CarouselImg1} alt="First slide" />
+            <img
+               className="d-block w-100"
+               src={CarouselImg1}
+               alt="First slide"
+            />
             <Carousel.Caption>
                <p>
-                  To, że wiesz o tym, że jesteś zakażony HIV daje Ci możliwość kontrolowania sytuacji. Ale żeby
-                  kontrolować sytuację musisz też trochę wiedzieć o HIV i AIDS.
+                  To, że wiesz o tym, że jesteś zakażony HIV daje Ci możliwość
+                  kontrolowania sytuacji. Ale żeby kontrolować sytuację musisz
+                  też trochę wiedzieć o HIV i AIDS.
                </p>
             </Carousel.Caption>
          </Carousel.Item>
 
          <Carousel.Item>
-            <img className="d-block w-100" src={CarouselImg2} alt="First slide" />
+            <img
+               className="d-block w-100"
+               src={CarouselImg2}
+               alt="First slide"
+            />
             <Carousel.Caption>
                <p>
-                  Dzięki temu wiesz, że możesz monitorować przebieg zakażenia, zwolnić jego postęp i zapobiec
-                  wystąpieniu chorób związanych za infekcją HIV.
+                  Dzięki temu wiesz, że możesz monitorować przebieg zakażenia,
+                  zwolnić jego postęp i zapobiec wystąpieniu chorób związanych
+                  za infekcją HIV.
                </p>
             </Carousel.Caption>
          </Carousel.Item>
 
          <Carousel.Item>
-            <img className="d-block w-100" src={CarouselImg3} alt="First slide" />
+            <img
+               className="d-block w-100"
+               src={CarouselImg3}
+               alt="First slide"
+            />
             <Carousel.Caption>
-               <p>Możesz uchronić przed zakażaniem Twoją partnerkę/partnera seksualnego.</p>
+               <p>
+                  Możesz uchronić przed zakażaniem Twoją partnerkę/partnera
+                  seksualnego.
+               </p>
             </Carousel.Caption>
          </Carousel.Item>
 
          <Carousel.Item>
-            <img className="d-block w-100" src={CarouselImg4} alt="First slide" />
+            <img
+               className="d-block w-100"
+               src={CarouselImg4}
+               alt="First slide"
+            />
             <Carousel.Caption>
                <p>Możesz mieć zdrowe dzieci.</p>
             </Carousel.Caption>
